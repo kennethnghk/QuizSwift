8056dfb174d06541bd1e21981a0e56870c47a49f
Kenneth Ng <kenneth.ng@lifehikes.com>
2022-11-30 17:49:56 +0800

    Update progress bar progress and add color to button

--- QuizSwift/ViewController.swift
+++ QuizSwift/ViewController.swift
@@ -17,7 +17,8 @@ class ViewController: UIViewController {
     let questions : [Question] = [
         Question(text: "1 + 3 = 4", answer: "True"),
         Question(text: "4 + 5 = 0", answer: "False"),
-        Question(text: "8 x 9 = 74", answer: "False")
+        Question(text: "8 x 9 = 74", answer: "False"),
+        Question(text: "8 x 1 = 8", answer: "True")
     ]
     var currentQuestion : Int = 0
     
@@ -31,7 +32,8 @@ class ViewController: UIViewController {
         
         let userAnswer = sender.titleLabel!.text!
         if questions[currentQuestion].answer == userAnswer {
-            print("Correct!")
+            
+            sender.backgroundColor = UIColor.green
             
             if currentQuestion + 1 < questions.count {
                 currentQuestion += 1
@@ -39,14 +41,19 @@ class ViewController: UIViewController {
                 currentQuestion = 0
             }
             
-            updateUI()
+            
         } else {
-            print("Wrong!")
+            sender.backgroundColor = UIColor.red
         }
+        
+        Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
     }
     
-    func updateUI() {
+    @objc func updateUI() {
         questionLabel.text = questions[currentQuestion].text
+        trueButton.backgroundColor = UIColor.clear
+        falseButton.backgroundColor = UIColor.clear
+        progressBar.progress = Float(currentQuestion + 1) / Float(questions.count)
     }
     
 }
